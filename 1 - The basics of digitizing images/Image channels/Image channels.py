8aade7c0e8602c8c5909a73eb6e24bc3281d0bf5
Mikhail Bokov <mixail.bokov9@gmail.com>
2021-01-28 19:57:11 +0700

    added comments

--- 1 - The basics of digitizing images/Image channels/Image channels.py
+++ 1 - The basics of digitizing images/Image channels/Image channels.py
@@ -1,11 +1,13 @@
 import cv2
 
-path = r'/Users/datmcl/Desktop/learning-basics-opencv/1 - The basics of digitizing images/scream.jpg'
-folderPath = r'/Users/datmcl/Desktop/learning-basics-opencv/1 - The basics of digitizing images/Image channels/'
-img = cv2.imread(path)
+path = r'/Users/datmcl/Desktop/learning-basics-opencv/1 - The basics of digitizing images/scream.jpg' # path to image
+folderPath = r'/Users/datmcl/Desktop/learning-basics-opencv/1 - The basics of digitizing images/Image channels/' # path to folder
 
-b, g, r = cv2.split(img)
+img = cv2.imread(path) # reading image
 
+b, g, r = cv2.split(img) # splitting image to channels R, G, B
+
+# saving
 cv2.imwrite(folderPath + 'screamB.jpg', b)
 cv2.imwrite(folderPath + 'screamG.jpg', g)
 cv2.imwrite(folderPath + 'screamR.jpg', r)
